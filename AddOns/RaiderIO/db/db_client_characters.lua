a30ae5a566aa378a47383f8ff971a60e5ab05d89
narcissus444 <narcissus9660@gmail.com>
2024-10-28 18:52:30 +0800

    Create db_client_characters.lua

---
+++ AddOns/RaiderIO/db/db_client_characters.lua
@@ -0,0 +1,241 @@
+--
+-- RaiderIO Client Data. Generated on Tue Sep 24 2024 16:09:25 GMT+0000 (Coordinated Universal Time)
+--
+local _, ns = ...
+ns.CLIENT_CHARACTERS = {
+  ["咩瑟拉伯爵茶-WorldTree"] = {
+    ["profile"] = {
+      ["name"] = "咩瑟拉伯爵茶",
+      ["realm"] = "WorldTree",
+      ["faction"] = "horde",
+      ["race"] = 70,
+      ["class"] = 13
+    },
+    ["mythic_keystone"] = {
+      ["all"] = {
+        ["score"] = 0,
+        ["best"] = nil,
+        ["bestFortified"] = nil,
+        ["bestTyrannical"] = nil,
+        ["runs"] = {
+
+        }
+      }
+    }
+  },
+  ["咩嚕嚼培根-WorldTree"] = {
+    ["profile"] = {
+      ["name"] = "咩嚕嚼培根",
+      ["realm"] = "WorldTree",
+      ["faction"] = "alliance",
+      ["race"] = 22,
+      ["class"] = 11
+    },
+    ["mythic_keystone"] = {
+      ["all"] = {
+        ["score"] = 1043,
+        ["best"] = {
+          ["zone_id"] = 14971,
+          ["level"] = 5,
+          ["upgrades"] = 1,
+          ["fraction"] = 0.804,
+          ["score"] = 227.3360598259322,
+          ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1784441-5-the-dawnbreaker?utm_source=client"
+        },
+        ["bestFortified"] = nil,
+        ["bestTyrannical"] = {
+          ["zone_id"] = 14971,
+          ["level"] = 5,
+          ["upgrades"] = 1,
+          ["fraction"] = 0.804,
+          ["score"] = 227.3360598259322,
+          ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1784441-5-the-dawnbreaker?utm_source=client"
+        },
+        ["runs"] = {
+          {
+            ["zone_id"] = 14882,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14883,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14938,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14954,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14971,
+            ["level"] = 5,
+            ["upgrades"] = 1,
+            ["fraction"] = 0.804,
+            ["score"] = 227.3360598259322,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1784441-5-the-dawnbreaker?utm_source=client"
+          },
+          {
+            ["zone_id"] = 14979,
+            ["level"] = 4,
+            ["upgrades"] = 2,
+            ["fraction"] = 0.677,
+            ["score"] = 217.1247677890375,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1784503-4-city-of-threads?utm_source=client"
+          },
+          {
+            ["zone_id"] = 15093,
+            ["level"] = 2,
+            ["upgrades"] = 3,
+            ["fraction"] = 0.57,
+            ["score"] = 180.0000030400017,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1784456-2-arakara-city-of-echoes?utm_source=client"
+          },
+          {
+            ["zone_id"] = 15103,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          }
+        }
+      }
+    }
+  },
+  ["咩嚕踢培根-WorldTree"] = {
+    ["profile"] = {
+      ["name"] = "咩嚕踢培根",
+      ["realm"] = "WorldTree",
+      ["faction"] = "alliance",
+      ["race"] = 7,
+      ["class"] = 10
+    },
+    ["mythic_keystone"] = {
+      ["all"] = {
+        ["score"] = 1732.5,
+        ["best"] = {
+          ["zone_id"] = 14883,
+          ["level"] = 5,
+          ["upgrades"] = 2,
+          ["fraction"] = 0.729,
+          ["score"] = 230.16807050382155,
+          ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/2020974-5-the-stonevault?utm_source=client"
+        },
+        ["bestFortified"] = nil,
+        ["bestTyrannical"] = {
+          ["zone_id"] = 14883,
+          ["level"] = 5,
+          ["upgrades"] = 2,
+          ["fraction"] = 0.729,
+          ["score"] = 230.16807050382155,
+          ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/2020974-5-the-stonevault?utm_source=client"
+        },
+        ["runs"] = {
+          {
+            ["zone_id"] = 14882,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14883,
+            ["level"] = 5,
+            ["upgrades"] = 2,
+            ["fraction"] = 0.729,
+            ["score"] = 230.16807050382155,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/2020974-5-the-stonevault?utm_source=client"
+          },
+          {
+            ["zone_id"] = 14938,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14954,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          },
+          {
+            ["zone_id"] = 14971,
+            ["level"] = 5,
+            ["upgrades"] = 1,
+            ["fraction"] = 0.811,
+            ["score"] = 227.07418434937364,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1370304-5-the-dawnbreaker?utm_source=client"
+          },
+          {
+            ["zone_id"] = 14979,
+            ["level"] = 6,
+            ["upgrades"] = 0,
+            ["fraction"] = 1.15,
+            ["score"] = 214.35870150342765,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/1467195-6-city-of-threads?utm_source=client"
+          },
+          {
+            ["zone_id"] = 15093,
+            ["level"] = 4,
+            ["upgrades"] = 1,
+            ["fraction"] = 0.914,
+            ["score"] = 208.20663698314104,
+            ["url"] = "https://raider.io/mythic-plus-runs/season-tww-1/2020979-4-arakara-city-of-echoes?utm_source=client"
+          },
+          {
+            ["zone_id"] = 15103,
+            ["level"] = 0,
+            ["upgrades"] = 0,
+            ["fraction"] = 0,
+            ["score"] = 0,
+            ["url"] = nil
+          }
+        }
+      }
+    }
+  },
+  ["咩嚕烤培根-WorldTree"] = {
+    ["profile"] = {
+      ["name"] = "咩嚕烤培根",
+      ["realm"] = "WorldTree",
+      ["faction"] = "alliance",
+      ["race"] = 11,
+      ["class"] = 8
+    },
+    ["mythic_keystone"] = {
+      ["all"] = {
+        ["score"] = 0,
+        ["best"] = nil,
+        ["bestFortified"] = nil,
+        ["bestTyrannical"] = nil,
+        ["runs"] = {
+
+        }
+      }
+    }
+  }
+}
+
