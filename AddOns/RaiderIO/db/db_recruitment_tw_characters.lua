89d713c367fbfdcfb5b7ccc1cb1ed28f047b1399
narcissus444 <narcissus9660@gmail.com>
2024-10-28 18:52:34 +0800

    Create db_recruitment_tw_characters.lua

---
+++ AddOns/RaiderIO/db/db_recruitment_tw_characters.lua
@@ -0,0 +1,11 @@
+--
+-- Copyright (c) 2024 by Ludicrous Speed, LLC
+-- All rights reserved.
+--
+local provider={name=...,data=3,region="tw",date="2024-09-24T06:53:22Z",numCharacters=24840,db={}}
+local F
+
+F = function() provider.db["眾星之子"]={0,"魘塵"} end F()
+
+F = nil
+RaiderIO.AddProvider(provider)
